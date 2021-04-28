40f84a26f4749aa944438ff96623d7358f4643da
Myles Starr <mylesstarr@gmail.com>
2021-04-27 21:16:23 -0400

    commit tree 60433490251bc84f6a1a5cc6762560dc84432dd2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    {return(`This Saturday, I want to ${activity}!`)}
+}
+
+const mondayWork = function(toDo="go to the office")
+    {return (`This Monday, I will ${toDo}.`)
+    }
+
+// const wrapAdjective = function(defaultParameter="special"){
+//     return
+// }
+    
+function wrapAdjective(flair="*") {
+    const inner = function(adjective="special",){
+        return (`You are ${flair}${adjective}${flair}!`)
+    }
+return inner
+}
+
